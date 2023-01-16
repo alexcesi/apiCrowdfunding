f4682f6a0e26ce2c0c5943baa731dca29cc8dd3f
alexcesi <alexvesier@gmail.com>
2023-01-16 19:03:30 +0100

    .env gitignore

--- src/main/java/com/apiCrowdfunding/javaProject/config/ApplicationConfig.java
+++ src/main/java/com/apiCrowdfunding/javaProject/config/ApplicationConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.apiCrowdfunding.javaProject.repository.UserRepository;
 
+
 import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Configuration
